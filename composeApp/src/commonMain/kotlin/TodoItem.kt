17fa098e316dcd6274b164fc6ec414ecfe4c12fd
rudradave1 <112609726+rudrayore@users.noreply.github.com>
2024-06-07 09:52:35 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/TodoItem.kt
@@ -0,0 +1,6 @@
+
+internal data class TodoItem(
+    val id: Long = 0L,
+    val text: String = "",
+    val isDone: Boolean = false
+)
---
+++ composeApp/src/wasmJsMain/kotlin/main.android.kt
@@ -0,0 +1,5 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable fun MainView() = RootContent(Modifier.fillMaxSize())
